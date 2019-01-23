54b32edbd72d42951903979f1f4d9b7c7df9d6c9
Don Hamilton III <donhamiltoniii@gmail.com>
2019-01-23 17:01:50 -0500

    Initial Commit

---
+++ src/test/java/cargarage/CarTest.java
@@ -0,0 +1,25 @@
+package cargarage;
+
+import org.junit.Test;
+
+public class CarTest {
+
+	@Test
+	public void shouldAccelerate() {
+		// Tests the accelerate() method in Car
+		// Arrange
+		// Act
+		// Assert
+	}
+	
+	@Test
+	public void shouldSlowDown() {
+		// Tests the brake() method in Car
+		// Arrange
+		// Act
+		// Assert
+	}
+	
+	@Test
+	public void should
+}
