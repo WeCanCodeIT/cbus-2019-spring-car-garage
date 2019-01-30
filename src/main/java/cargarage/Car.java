5dae0978a0123342399ad2d545867a506ac3c995
Don Hamilton III <donhamiltoniii@gmail.com>
2019-01-30 10:23:45 -0500

    Finish testing speed and fuel

--- src/main/java/cargarage/Car.java
+++ src/main/java/cargarage/Car.java
@@ -1,5 +1,26 @@
 package cargarage;
 
 public class Car {
+	
+	private int speed;
+	private int fuel;
+
+	public Car(int speed, int fuel) {
+		this.speed = speed;
+		this.fuel = fuel;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void accelerate(int speed) {
+		this.speed += speed;
+		this.fuel -= speed/2;
+	}
+
+	public int getFuel() {
+		return fuel;
+	}
 
 }
--- src/test/java/cargarage/CarTest.java
+++ src/test/java/cargarage/CarTest.java
@@ -1,23 +1,53 @@
 package cargarage;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CarTest {
 
+	/**
+	 * Code covered by this test:
+	 * 
+	 * Constructor that takes initial speed
+	 * Accessor method (Getter) for speed (car.getSpeed())
+	 * car.accelerate() method to add speed
+	 * 
+	 * Assertion: When the accelerate() method is called, the speed increases by provided parameter.
+	 * In this case, 10
+	 * 
+	 */
 	@Test
 	public void shouldAccelerate() {
 		// Tests the accelerate() method in Car
 		// Arrange
+		/**
+		 * New constructor
+		 */
+		Car car = new Car(0, 100);
+		
 		// Act
+		int initialSpeed = car.getSpeed();
+		car.accelerate(10);
+		int speedAfterAcceleration = car.getSpeed();
+		
 		// Assert
+		assertEquals(initialSpeed + 10, speedAfterAcceleration);
 	}
 	
 	@Test
 	public void shouldConsumeFuel() {
 		// Tests accelerate()
 		// Arrange
+		Car car = new Car(0, 100);
+		
 		// Act
+		int initialFuel = car.getFuel();
+		car.accelerate(10);
+		int fuelAfterAcceleration = car.getFuel();
+		
 		// Assert
+		assertEquals(initialFuel - 5, fuelAfterAcceleration);
 	}
 	
 	@Test
