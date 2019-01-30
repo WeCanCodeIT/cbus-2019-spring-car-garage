3d33437e3f4686a27c11ee06cf485038a0a76ed2
Don Hamilton III <donhamiltoniii@gmail.com>
2019-01-30 15:35:07 -0500

    Finish choice for listing cars

--- src/main/java/cargarage/Application.java
+++ src/main/java/cargarage/Application.java
@@ -14,12 +14,13 @@ public class Application {
 		while (isActive) {
 			System.out.println("1. Add a Car to the Garage");
 			System.out.println("2. Remove a Car from the Garage");
+			System.out.println("3. List all Cars");
 			
 			String userSelection = input.nextLine();
 			switch (userSelection) {
 			case "1":
 				System.out.println("Existing vins are as follows");
-				System.out.println(garage.getCars());
+				printAllCars(garage);
 				System.out.println("Please enter a unique vin for the car:");
 				String userVin = input.nextLine();
 				// All cars will be added with a starting speed of 0
@@ -28,12 +29,25 @@ public class Application {
 				break;
 			case "2":
 				System.out.println("Which car would you like to remove?:");
-				System.out.println(garage.getCars());
+				printAllCars(garage);
 				System.out.println("Enter vin of car to remove");
 				userVin = input.nextLine();
 				garage.removeCar(userVin);
 				System.out.println("Vin " + userVin + " was removed.");
+				break;
+			case "3":
+				System.out.println("Cars in garage are as follows");
+				printAllCars(garage);
+				break;
 			}
 		}
+	}
+
+	/**
+	 * Refactored out for abstractions sake
+	 * 
+	 */
+	private static void printAllCars(Garage garage) {
+		System.out.println(garage.getCars());
 	}
 }
