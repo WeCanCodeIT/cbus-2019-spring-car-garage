1190efe5ae6d461db92ca4172e277d595a0592fc
Don Hamilton III <donhamiltoniii@gmail.com>
2019-01-30 15:53:13 -0500

    Finish option to test drive a car

--- src/main/java/cargarage/Application.java
+++ src/main/java/cargarage/Application.java
@@ -9,13 +9,14 @@ public class Application {
 		Scanner input = new Scanner(System.in);
 		Garage garage = new Garage(new HashMap<>());
 		boolean isActive = true;
-		
+
 		System.out.println("Hello! Please select what you would like to do:");
 		while (isActive) {
 			System.out.println("1. Add a Car to the Garage");
 			System.out.println("2. Remove a Car from the Garage");
 			System.out.println("3. List all Cars");
-			
+			System.out.println("4. Test drive a car");
+
 			String userSelection = input.nextLine();
 			switch (userSelection) {
 			case "1":
@@ -38,6 +39,67 @@ public class Application {
 			case "3":
 				System.out.println("Cars in garage are as follows");
 				printAllCars(garage);
+				break;
+			case "4":
+				boolean isTestDriving = true;
+				System.out.println("Which car would you like to test drive?");
+				printAllCars(garage);
+				userVin = input.nextLine();
+				Car userCar = garage.getCar(userVin);
+				System.out.println("You chose car: " + userCar.getVin());
+
+				while (isTestDriving) {
+					System.out.println("What would you like to do next?:");
+					System.out.println("1. Turn on car");
+					System.out.println("2. Accelerate");
+					System.out.println("3. Brake");
+					System.out.println("4. Turn off car");
+					System.out.println("5. Return car");
+					String userResponse = input.nextLine();
+
+					switch (userResponse) {
+					case "1":
+						if (userCar.isOn()) {
+							System.out.println("Car is already on...");
+						} else {
+							userCar.toggleOn();
+							System.out.println("Car is on!");
+						}
+						break;
+					case "2":
+						if (!userCar.isOn()) {
+							System.out.println("Car must first be on...");
+						} else {
+							userCar.accelerate(20);
+							System.out.println("You accelerate by 20");
+						}
+						break;
+					case "3":
+						if (!userCar.isOn()) {
+							System.out.println("Car must first be on...");
+						} else {
+							// Stops car by applying brakes as much as the speed currently is
+							userCar.brake(userCar.getSpeed());
+							System.out.println("You stop the car");
+						}
+						break;
+					case "4":
+						if (!userCar.isOn()) {
+							System.out.println("Car is already off...");
+						} else {
+							userCar.toggleOn();
+							System.out.println("You turn off the car");
+						}
+						break;
+					case "5":
+						if (userCar.isOn()) {
+							System.out.println("Car must be off to return...");
+						} else {
+							isTestDriving = false;
+							System.out.println("You returned car: " + userCar.getVin());
+						}
+					}
+				}
 				break;
 			}
 		}
